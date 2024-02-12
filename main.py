ecf85fa0ac2e1d150d478fb6aee7a68d5449a02b
Alberto Montemiglio <71413117+alberto-montemiglio@users.noreply.github.com>
2024-02-12 17:21:02 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+'''
+
+VisuTimer
+The Visual Pomodoro Timer based on Raspberry Pi Pico and Badger 2040 W!
+
+
+Created by: 
+@alberto-montemiglio, 2024
+
+'''
+
+
+
+'''
+
+Screens:
+
+Pages: 
+On start: Start / Edit
+While Running: Press any button to -> Start / Stop / Change / Reset
+Change Screen: Blink 'WORK' / 'BREAK'. Use arrows to change. Select. Back to Default. Confirm. -> Back to While Running
+
+
+'''
