2260a894bf91c3f64174ed0b633dba5855b0c3af
alberto-montemiglio <albertomontemiglio@gmail.com>
2024-02-12 21:28:38 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -22,3 +22,21 @@ Change Screen: Blink 'WORK' / 'BREAK'. Use arrows to change. Select. Back to Def
 
 
 '''
+from picographics import PicoGraphics, DISPLAY_INKY_PACK, PEN_1BIT
+
+
+# Initiate the 296x128 mono E-ink Badger 2040 W display:
+
+
+class Screen:
+	def __init__(self, font):
+		self.display = PicoGraphics(display=DISPLAY_INKY_PACK, pen_type=PEN_1BIT)
+		self.WIDTH, self.HEIGHT = display.get_bounds()
+		self.display.set_font(bitmap8)
+		self.font_height = 8
+
+
+	def menu(self, menu_items, padding = 4):
+		text_y_position = self.HEIGHT-self.font_height+padding
+		for index, menu_item in enumerate(menu_items):
+			self.display.text(menu_item, (self.WIDTH/3*index), text_y_position)
