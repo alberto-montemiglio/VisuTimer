dc41c569a41014795813fe7f0d0ca2f2abebf0f4
alberto-montemiglio <albertomontemiglio@gmail.com>
2024-02-18 22:57:58 +0000

    everything works

--- main.py
+++ main.py
@@ -32,72 +32,7 @@ import micropython
 # Initiate the 296x128 mono E-ink Badger 2040 W display:
 
 class visuTimer:
-	def __init__(self):
 
-		# Create a badger2040 object from the badger2040 library:
-		self.badger = badger2040.Badger2040()
-		self.badger.set_update_speed(0)
-
-		# Instantiate PicoGraphics:
-		self.display = PicoGraphics(display=DISPLAY_INKY_PACK, pen_type=PEN_1BIT)
-
-		# Initialise Font
-		self.display.set_font('bitmap8')
-		self.font_height = 8
-
-		# Set Up Display
-		self.WIDTH, self.HEIGHT = self.display.get_bounds()
-		self.bottom_bar_height = 4*self.font_height
-		self.top_bar_height = self.HEIGHT - self.bottom_bar_height
-		self.screen_displayed = 'home'
-
-
-
-
-		# Initialise Buzzer Pin
-		self.buzzer_Pin = Pin(28, Pin.OUT)    # create output pin on GPIO28 (Which is free)
-		self.ledPin = Pin(22, Pin.OUT)	# create output pin on GPIO22 (Which has the LED)
-
-		# Define timer parameters
-		self.work_period = 0.25*60 # duration of work session in [s]
-		self.break_period = 0.25*60 # duration of break session in [s]
-		self.update_period = 5 # Screen update period in [s]
-		self.timer_stop_flag = 0 # Set timer to active
-		self.current_session = 'WORK' # Start with a work session
-
-		
-		button_map_function = {
-			'home': {
-				'butt_A': 'runTimer',
-				'butt_B': '',
-				'butt_C': ''
-				},
-			'run': 	{
-				'butt_A': 'pauseScreen',
-				'butt_B': 'pauseScreen',
-				'butt_C': 'pauseScreen'},
-			'pause': {
-				'butt_A': 'home',
-				'butt_B': 'continue',
-				'butt_C': ''},
-		}
-
-		# Interrupts:
-		# button_A = Pin(badger2040.BUTTON_A,Pin.IN,Pin.PULL_UP)		
-		# button_A.irq(handler=None, trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING)
-
-	# def __button_A(self):
-	# 	self.UI_map[self.screen_displayed][button_A]
-		
-	# 	'''
-		# if screen_displayed = home 
-		# UI_map = {
-		# 	home: {button_A: 'start', '', ''],
-		# 	pause: ['Restart', 'Continue', '']
-		# 	running: ['pause', 'pause', 'pause']
-		# }
-
-		# '''
 	def __drawProgressBar(self, start, width):
 
 		# Speed up screen updating
@@ -115,7 +50,7 @@ class visuTimer:
 		self.display.update()
 		
 		# Revert to normal update speed
-		self.badger.set_update_speed(3)
+		self.badger.set_update_speed(0)
 
 
 
@@ -130,47 +65,54 @@ class visuTimer:
 
 
 	def __increaseTimer(self, timer):
-		print('Maybe Increasing Timer')
 		if not self.timer_stop_flag: # Run the following only if the timer has not been paused
-			print('Increasing Timer')
 			self.time_elapsed = self.time_elapsed + self.update_period
 			if self.current_session == 'BREAK':
-				print('in break')
 				progress_bar_width = round(self.WIDTH*self.time_elapsed/self.work_period)
 				self.__drawProgressBar(start = self.WIDTH - progress_bar_width, width = progress_bar_width)
 				if self.time_elapsed >= self.break_period:
-					print('finished break')
 					self.ringTimer()
 					self.current_session = 'WORK'
+					self.display_pomodoro_screen(self.current_session)
 					self.time_elapsed = 0
 			else: 
-				print('in work')
 				progress_bar_width = round(self.WIDTH*self.time_elapsed/self.work_period)
 				self.__drawProgressBar(start = 0, width = progress_bar_width)
 				if self.time_elapsed >= self.work_period:
-					print('finished work')
 					self.ringTimer()
 					self.current_session = 'BREAK'
+					self.display_pomodoro_screen(self.current_session)
 					self.time_elapsed = 0
 
 
 
 	def startTimer(self):
+
+		self.current_screen = 'run'
+
 		# Show Timer Page
 		self.display_pomodoro_screen(self.current_session)
 
 		# Start Timer
 		self.time_elapsed = 0 # Minutes Passed
-		print("declaring a timer")
 		self.tim = Timer().init( period=self.update_period*1000, callback=self.__increaseTimer ) # Update Timer every 10 sec
 
-	def stopTimer(self):
+	def pauseTimer(self):
+
+		self.current_screen = 'pause'
+		self.display_pause_screen()
 		self.timer_stop_flag = 1
 
 	def continueTimer(self):
+
+		self.current_screen = 'run'
+		self.display_pomodoro_screen(self.current_session)
 		self.timer_stop_flag = 0
 
 	def endTimer(self):
+
+		self.current_screen = 'home'
+		self.display_home_screen()
 		self.tim.deinit()
 
 	
@@ -211,7 +153,7 @@ class visuTimer:
 	def display_pause_screen(self):
 		self.clear_screen()		
 		self.__draw_logo()
-		self.__draw_menu(['Restart', 'Continue', ''])
+		self.__draw_menu(['End Timer', 'Continue', ''])
 		self.display.update()
 
 	def display_pomodoro_screen(self, instruction):
@@ -230,9 +172,96 @@ class visuTimer:
 
 		self.display.update()
 
+	def button_A_ISR(self, buttonInterrupt):
+		if self.current_screen == 'home': self.startTimer()
+		elif self.current_screen == 'run': self.pauseTimer()
+		elif self.current_screen == 'pause': self.endTimer()
+		else: pass
+
+	def button_B_ISR(self, buttonInterrupt):
+		if self.current_screen == 'home': pass
+		elif self.current_screen == 'run': self.pauseTimer()
+		elif self.current_screen == 'pause': self.continueTimer()
+		else: pass
+
+	def button_C_ISR(self, buttonInterrupt):
+		if self.current_screen == 'home': pass
+		elif self.current_screen == 'run': self.pauseTimer()
+		elif self.current_screen == 'pause': pass
+		else: pass
+
+
+	def __init__(self):
+
+		# Create a badger2040 object from the badger2040 library:
+		self.badger = badger2040.Badger2040()
+		self.badger.set_update_speed(0)
+
+		# Instantiate PicoGraphics:
+		self.display = PicoGraphics(display=DISPLAY_INKY_PACK, pen_type=PEN_1BIT)
+
+		# Initialise Font
+		self.display.set_font('bitmap8')
+		self.font_height = 8
+
+		# Set Up Display
+		self.WIDTH, self.HEIGHT = self.display.get_bounds()
+		self.bottom_bar_height = 4*self.font_height
+		self.top_bar_height = self.HEIGHT - self.bottom_bar_height
+		self.screen_displayed = 'home'
+
+
+
+
+		# Initialise Buzzer Pin
+		self.buzzer_Pin = Pin(28, Pin.OUT)    # create output pin on GPIO28 (Which is free)
+		self.ledPin = Pin(badger2040.LED, Pin.OUT)	# create output pin on GPIO22 (Which has the LED)
+
+		# Define timer parameters
+		self.work_period = 0.25*60 # duration of work session in [s]
+		self.break_period = 0.25*60 # duration of break session in [s]
+		self.update_period = 5 # Screen update period in [s]
+		self.timer_stop_flag = 0 # Set timer to active
+		self.current_session = 'WORK' # Start with a work session
+
+		self.current_screen = 'home'
+
+		# self.button_map_function = {
+		# 	'home': {
+		# 		'butt_A': self.startTimer,
+		# 		'butt_B': lambda *args, **kwargs: None,
+		# 		'butt_C': lambda *args, **kwargs: None
+		# 		},
+		# 	'run': 	{
+		# 		'butt_A': self.pauseTimer,
+		# 		'butt_B': self.pauseTimer,
+		# 		'butt_C': self.pauseTimer
+		# 		},
+		# 	'pause': {
+		# 		'butt_A': self.endTimer,
+		# 		'butt_B': self.continueTimer,
+		# 		'butt_C': lambda *args, **kwargs: None
+		# 		},
+		# }
+
+		# def buttonA_ISR(self):
+
+		# Interrupts:
+		self.button_A = Pin(badger2040.BUTTON_A,Pin.IN,Pin.PULL_DOWN)		
+		self.button_A.irq(handler=self.button_A_ISR, trigger=Pin.IRQ_RISING)
+
+		# self.button_A = Pin(badger2040.BUTTON_A,Pin.IN,Pin.PULL_DOWN)		
+		# self.button_A.irq(handler=self.startTimer, trigger=Pin.IRQ_RISING)
+
+		self.button_B = Pin(badger2040.BUTTON_B,Pin.IN,Pin.PULL_DOWN)		
+		self.button_B.irq(handler=self.button_B_ISR, trigger=Pin.IRQ_RISING)
+
+		self.button_C = Pin(badger2040.BUTTON_C,Pin.IN,Pin.PULL_DOWN)		
+		self.button_C.irq(handler=self.button_C_ISR, trigger=Pin.IRQ_RISING)
 
 
 
 micropython.alloc_emergency_exception_buf(100)
 visuTimer = visuTimer()
-visuTimer.startTimer()
+visuTimer.display_home_screen()
+# visuTimer.startTimer()
